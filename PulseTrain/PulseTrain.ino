0610013d0c158fcc066beda1543a6906a5563036
Luke-Metcalfe <149692080+Luke-Metcalfe@users.noreply.github.com>
2024-08-02 09:28:32 +0200

    Updated signal generation to RMT.
    Updated signal generation to use RMT peripheral instead of previous, time-based GPIO approach, to free CPU.

--- PulseTrain/PulseTrain.ino
+++ PulseTrain/PulseTrain.ino
@@ -4,16 +4,26 @@
 #include <cstddef>
 #include "Wheel.h"
 
-// Bit masks for setting/clearing GPIO register
-const unsigned long pin0 = 1ul << 0;  // ...00001
-const unsigned long pin1 = 1ul << 1;  // ...00010
-const unsigned long pin2 = 1ul << 2;
-const unsigned long pin4 = 1ul << 4;
-const unsigned long pin5 = 1ul << 5;
-const unsigned long pin15 = 1ul << 15;
-const unsigned long pin17 = 1ul << 17;
-const unsigned long pin18 = 1ul << 18;
-const unsigned long pin19 = 1ul << 19;
+const unsigned long pin0 = 0;  // ...00001
+const unsigned long pin1 = 1;  // ...00010
+const unsigned long pin2 = 2;
+const unsigned long pin4 = 4;
+const unsigned long pin5 = 5;
+const unsigned long pin15 = 15;
+const unsigned long pin17 = 17;
+const unsigned long pin18 = 18;
+const unsigned long pin19 = 19;
+
+// duration 0, level 0, duration 1, level 1
+rmt_data_t zero = { 25, 1, 25, 0 };  // 400kHz = 2.5us per cycle, 10 cycles  = 25us // 1MHz = 1us/cycle
+rmt_data_t one = { 25, 0, 25, 1 };
+rmt_data_t blank = { 1, 0, 1, 0 };  // duration must be min 1 cycle, otherwise this appears as an EOF.
+rmt_data_t EndOfFrame = { 0, 0, 0, 0 };
+
+rmt_data_t pulse = { 25, 1, 25, 1 };
+rmt_data_t pause1 = { 12, 0, 13, 0 };
+rmt_data_t pause2 = { 12, 0, 13, 0 };
+rmt_data_t currentWheelData[13];
 
 // Sensor pulse width
 int Tp = 50;  // microseconds
@@ -21,12 +31,12 @@ int Tp = 50;  // microseconds
 float teeth = 48.0;
 float wheelCirc = 1950.0;
 
-Wheel wheelArray[4] = { 
+Wheel wheelArray[4] = {
 
   Wheel(pin4, pin15, 190.0),  // FL
   Wheel(pin5, pin19, 191.1),  // FR
   Wheel(pin0, pin17, 192.2),  // RL
-  Wheel(pin2, pin18, 193.3) // RR
+  Wheel(pin2, pin18, 193.3)   // RR
 
 };
 
@@ -35,131 +45,51 @@ void setup() {
   Serial.begin(115200);
   delay(2000);
 
-  //ESP32 config io
-  gpio_config_t io_conf;
-  //ESP32 config io to output
-  io_conf.mode = GPIO_MODE_OUTPUT;
-  //Bitmask GPIOs as Output
-  io_conf.pin_bit_mask = pin4 | pin5 | pin0 | pin2 | pin15 | pin19 | pin17 | pin18;
-  //Set BitMask
-  gpio_config(&io_conf);
+  for (int i = 0; i < 4; i++) {
+    Wheel& w = wheelArray[i];
+    rmtInit(w.getPin1(), RMT_TX_MODE, RMT_MEM_NUM_BLOCKS_1, 1000000);
+  }
+
+  Wheel& w1 = wheelArray[0];
+  w1.setSpeed(200.0);
+  updatePulse(w1);
+  Wheel& w2 = wheelArray[1];
+  w2.setSpeed(210.0);
+  updatePulse(w2);
+  Wheel& w3 = wheelArray[2];
+  w3.setSpeed(220.0);
+  updatePulse(w3);
+  Wheel& w4 = wheelArray[3];
+  w4.setSpeed(230.0);
+  updatePulse(w4);
 }
 
 void loop() {
-  checkTransitions();
+  
 }
 
-void checkTransitions() {
-  for (int i = 0; i < 4; i++) {
-    Wheel &w = wheelArray[i];
-    if (micros() >= w.getNextTransition()) {  // current time exceeds next transition time
-      newTransition(w);
+
+void updatePulse(Wheel& w) {
+  currentWheelData[0] = pulse;     // insert large pulse
+  currentWheelData[1] = pause1;    // insert pause 1
+  for (int i = 2; i <= 10; i++) {  // insert serial data
+    if (i - 2 > w.availDataBits - 1) {
+      currentWheelData[i] = blank;  // insert blank bit if serial data exceeds available bits
+    } else if (w.serialData[i - 2] == 1) {
+      currentWheelData[i] = one;
+    } else if (w.serialData[i - 2] == 0) {
+      currentWheelData[i] = zero;
     }
   }
-}
-
-void newTransition(Wheel& w) {
-
-  switch (w.getZone()) {
-
-    case 0:                                       // Large Pulse
-      if (w.getSpeed() == 0.0){
-        GPIO.out_w1ts = w.getPin2();  // set
-      }
-      else {
-        GPIO.out_w1ts = w.getPin1() | w.getPin2();  // set
-      }
-      w.incrNextTransition(Tp);
-      w.setZone(1);
-      break;
-
-    case 1:                                       // Pause 1
-      GPIO.out_w1tc = w.getPin1() | w.getPin2();  // clear
-      w.incrNextTransition(Tp/2);
-
-      // Entering data transmission
-      w.setZone(2);
-      w.setTransmissionHalf(1);
-      w.setCurrentDataBit(0);
-      break;
-
-    case 2:  // Data Transmission
-
-      switch (w.getTransmissionHalf()) {
-        case 1:
-          firstHalfTransition(w);
-          w.incrNextTransition(Tp/2);
-          w.setTransmissionHalf(2);
-          break;
-
-        case 2:
-          secondHalfTransition(w);
-
-          if (w.getCurrentDataBit() >= w.getAvailDataBits() - 1) {  // last data bit, short transition time
-            w.incrNextTransition(Tp/2);
-
-            // exit data transmission
-            if (w.getAvailDataBits() < 9) {
-              GPIO.out_w1tc = w.getPin2();
-              w.setZone(100);  // skip zone 3, no pause 2
-            } else {
-              w.setZone(3);  // add pause 2
-            }
-            w.setTransmissionHalf(100);
-            w.setCurrentDataBit(100);
-
-          } else if (w.getSerialData(w.getCurrentDataBit()) ^ w.getSerialData(w.getCurrentDataBit() + 1)) {  // xor: if current bit diff to next, next transition occurs later.
-            w.incrNextTransition(Tp);
-            w.setTransmissionHalf(2); // skip ahead to 2nd half transition of next bit, 50us later
-
-          } else {  // adjacent bits have same value, short transition
-            w.incrNextTransition(Tp/2);
-            w.setTransmissionHalf(1); // move to 1st half of next bit, 25us later.
-          }
-
-          w.setCurrentDataBit(w.getCurrentDataBit() + 1);  // move to next data bit
-          break;
-      }
-      break;
-
-    case 3:  // Pause 2
-      GPIO.out_w1tc = w.getPin2();
-      w.incrNextTransition(Tp/2);
-      w.setZone(100);
-      break;
-
-    case 100:  // Transition out of NULL zone
-      w.setZone(0);
-      w.incrNextTransition( w.getPeriod()/2 - (w.getAvailDataBits()*Tp + (1.5*Tp)) ); // remaining time in period, after pulse, pause, and transmission of available bits
-
-      if (w.getAvailDataBits() >= 9) {    // NB!! Investigate this
-        w.incrNextTransition(Tp/2); // add pause 2 if wheel period is large enough (all bits are available)
-      }
-
-      break;
+  // Insert Pause 2
+  if (w.availDataBits < 9) {       // No pause 2
+    currentWheelData[11] = blank;  // insert blank bit
+  } else {
+    currentWheelData[11] = pause2;  // insert pause 2
   }
-}
+  float rem = w.period / 2 - (w.availDataBits * Tp + 1.5 * Tp);
+  currentWheelData[12] = { rem / 2, 0, rem / 2, 0 };  // remaining time until next large pulse
+  currentWheelData[13] = EndOfFrame;                  // End Of Frame
 
-void firstHalfTransition(Wheel& w) {
-  switch (w.getSerialData(w.getCurrentDataBit())) {
-    case 1:
-      GPIO.out_w1tc = w.getPin2();  // clear
-      break;
-
-    case 0:
-      GPIO.out_w1ts = w.getPin2();  // set
-      break;
-  }
-}
-
-void secondHalfTransition(Wheel& w) {
-  switch (w.getSerialData(w.getCurrentDataBit())) {
-    case 1:
-      GPIO.out_w1ts = w.getPin2();  // set
-      break;
-
-    case 0:
-      GPIO.out_w1tc = w.getPin2();  // clear
-      break;
-  }
+  rmtWriteLooping(w.getPin1(), currentWheelData, RMT_SYMBOLS_OF(currentWheelData));
 }
--- PulseTrain/Wheel.h
+++ PulseTrain/Wheel.h
@@ -38,23 +38,30 @@ public:
   void calcDataBits();
   void calcParity();
 
+  char serialData[9] = { 1, 1, 0, 1, 1, 0, 1, 0, 1 };
+  //                    LR, M, DE, GDR, DR, LM0, LM1, LM2, P - see documentation
+  int availDataBits; // available data bits based on wheel frequency
+  float period; // us
+
 private:
   unsigned long pin1; // Pin for triggering large pulse
   unsigned long pin2; // Pin for triggering small pulse
 
   float speed;  // km/h
-  float period; // us
+  //float period; // us
   float freq;   // Hz
 
+  /*
   char serialData[9] = { 1, 1, 0, 1, 1, 0, 1, 0, 1 };
   //                    LR, M, DE, GDR, DR, LM0, LM1, LM2, P - see documentation
-  
+  */
+
   // Data Transmission Variables
   long int nextTransition;  // time stamp of next transition
   int zone; // 0 in pulse, 1 in pause 1, 2 in data transmission, 3 in pause 2, 100 when resting
   int currentDataBit; // 100 not in data transmission, 0-8 when in data
   int transmissionHalf; // 100 not in data transmission, 1 first half, 2 second half
-  int availDataBits; // available data bits based on wheel frequency
+  //int availDataBits; // available data bits based on wheel frequency
 
   // Constants for speed/freq/period calculations
   float teeth = 48.0; // teeth/poles on encoder track 
